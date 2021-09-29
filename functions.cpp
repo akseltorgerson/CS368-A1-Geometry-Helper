60436aff544f6af4b6c03fa250bba25cdc48a301
akseltorgerson <akseltorgerson@gmail.com>
2021-09-28 22:42:16 -0500

    added initial files

---
+++ functions.cpp
@@ -0,0 +1,29 @@
+#define _USE_MATH_DEFINES
+
+#include <math.h>
+using namespace std;
+
+float AreaOfCircle(const float& radius) {
+  return (M_PI * (radius^2));
+}
+
+float AreaOfTriangle(const float& width, const float& height) {
+  return (0.5 * width * height);
+}
+
+float AreaOfRectangle(const float& width, const float& height) {
+  return (width * height);
+}
+
+float VolumeOfSphere(const float& radius) {
+  return ((float)(4/3 * M_PI * (radius^3)));
+}
+
+float VolumeOfCone(const float& radius, const float& height) {
+  return ((1/3) * M_PI * (radius^2) * height);
+}
+
+float VolumeOfBox(const float& width, const float& height, const float& length) {
+  return (width * height * length);
+}
+
---
+++ main.cpp
@@ -0,0 +1,26 @@
+// Aksel Torgerson
+// atorgerson
+
+#include <iostream>
+#include <functions.h>
+
+using namespace std;
+
+int main() {
+
+
+
+
+  return 0;
+}
+
+void mainMenu() {
+  cout << "Welcome to Geometry Helper!" << endl;
+  cout << "[1] Calculate a 2D area" << endl;
+  cout << "[2] Calculate a 3D volume" << endl;
+  cout << "[3] Exit" << endl;
+  cout << "Please enter a menu item: ";
+}
+
+void 
+
